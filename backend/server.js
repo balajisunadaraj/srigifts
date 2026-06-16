7556e1f77b6adf844c1c286e19f2529747ebf36f
balajisunadaraj <24uca228@anjaconline.org>
2026-06-16 20:09:47 +0530

    log check

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+console.log("=== DEPLOY TEST 12345 ===");
 require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
